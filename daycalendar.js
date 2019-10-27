8f6f722d224345292d815250e9ff30cfbefd316c
David Vega <dvega920@gmail.com>
2019-10-27 17:38:47 -0400

    trouble with colors and local storage

--- daycalendar.js
+++ daycalendar.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
 
     var x = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+    var currentTime = moment().format("H");
+
     //moment.js to put current date on jumbotron
     var currentDate = moment().format("dddd, MMMM Do YYYY");
     $("#currentDay").text(currentDate);
@@ -9,86 +11,65 @@ $(document).ready(function () {
     //forEach loops through x[], pushes "time" elements to new startTime[] in the "00 AM/PM" format
     x.forEach(function (time, index) {
         var startTime = [];
-        // currTime.push(time);
         startTime.push(moment().hour(time).format("h A"));
+        // console.log(parseInt(startTime));
+        // console.log(moment().hour());
 
-        //Declare new var(s) and initialize to new el's needed        
+        //Declare new var(s) and initialize to new el's        
         var newDiv = $("<div>");
         var newSpan = $("<span>");
         var newTextInput = $("<input>");
         var newBtn = $("<button>");
 
-
         //appends new el's to container, parent or sibling respectively. Adds B/S classes to new el's 
         $(".container").append(newDiv);
+        $(".container").addClass("mb-5");
         newDiv.addClass("time-block input-group input-group-prepend");
         newDiv.append(newSpan);
-        newSpan.addClass("start-time input-group-text");
+        newSpan.addClass("input-group-text");
+        newSpan.addClass("start-time" + index)
         newSpan.text(startTime);
         newDiv.append(newTextInput);
+        newTextInput.attr("type", "text");
         newTextInput.addClass("form-control")
-        newTextInput.attr("id", "text-input" + index)
+        newTextInput.addClass("input-text");
         newDiv.append(newBtn);
         newBtn.addClass("btn btn-outline-secondary");
-        newBtn.attr("id", "btn" + index);
 
+        for (var i = 9; i < 17; i++) {
+            if (i < currentTime) {
+                newTextInput.addClass("past");
+            } else if (i > currentTime) {
+                newTextInput.addClass("present");
+            } else {
+                newTextInput.addClass("future");
+            }
 
-    });
-    /* END OF FOREACH()*/
-
-
-    //event 
-    $("#btn0").on("click", function (event) {
-        event.preventDefault();
-
-        var textInput0 = $("#text-input0");
-        var textVal0 = textInput0.val();
-        localStorage.setItem("text-input0", textVal0);
-        localStorage.getItem(textInput0);
+        }
 
     })
-    // var textInput1 = $("#text-input1");
-    // var textInput2 = $("#text-input2");
-    // var textInput3 = $("#text-input3");
-    // var textInput4 = $("#text-input4");
-    // var textInput5 = $("#text-input5");
-    // var textInput6 = $("#text-input6");
-    // var textInput7 = $("#text-input7");
-    // var textInput8 = $("#text-input8");
+    /* END OF FOREACH()*/
 
-    // var textVal1 = textInput1.val();
-    // var textVal2 = textInput2.val();
-    // var textVal3 = textInput3.val();
-    // var textVal4 = textInput4.val();
-    // var textVal5 = textInput5.val();
-    // var textVal6 = textInput6.val();
-    // var textVal7 = textInput7.val();
-    // var textVal8 = textInput8.val();
-
-    // var textVal = $(this).textInput.val();
+    //Set array to local storage
+    localStorage.setItem("timeblocks", x);
 
 
+    var ls = localStorage.getItem("timeblocks");
+    $(".input-text").text(ls);
 
+    $("button").on("click", function () {
+        event.preventDefault();
 
-    // if (textVal){
-    //     alert("you've enteredf input");
-    // } else {
-    //     alert("no inputs");
-    // }
-
-
-
+        var value = $(".input-text").val();
+        // console.log(value);
+        localStorage.setItem('input', value);
+        localStorage.getItem('input');
+    });
 
     //updated width of time block span and changed btn text to "SAVE"
     // Consider using fontawesome icon for save feature
     $(".input-group-text").css("width", "100px");
     $("button").text("SAVE"); // update with icon from font-awesome
-
-
-
-
-
-
 
 })
 
